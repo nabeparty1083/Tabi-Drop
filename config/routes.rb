0273b691d9efb678f0fdcb586cde6ef2af4c28b0
nabeparty1083 <jdabcryo0803@gmail.com>
2026-09-08 00:12:57 +0900

    ログイン・ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "mypage", to: "mypages#show", as: :mypage
+
+
   devise_for :users
   root "static_pages#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,31 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
+  setup do
+    @user = User.create!(
+      email: "top-test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  test "未ログインの場合はログインと新規登録を表示する" do
     get root_url
+
     assert_response :success
+    assert_select "a[href=?]", new_user_session_path, text: "ログイン"
+    assert_select "a[href=?]", new_user_registration_path, text: "新規登録"
+    assert_select "a[href=?]", destroy_user_session_path, count: 0
+  end
+
+  test "ログイン済みの場合はマイページとログアウトを表示する" do
+    sign_in @user
+
+    get root_url
+
+    assert_response :success
+    assert_select "a[href=?]", mypage_path, text: "マイページ"
+    assert_select "a[href=?]", destroy_user_session_path, text: "ログアウト"
+    assert_select "a[href=?]", new_user_session_path, count: 0
   end
 end
